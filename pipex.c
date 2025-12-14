c12ae3cd80288d8dd07e79f331890aa646e8060e
sudoDodo <guilbertmael@gmai.com>
2025-12-14 17:00:06 +0100

    fd

--- pipex.c
+++ pipex.c
@@ -1,78 +1,156 @@
 #include "pipex.h"
-#include <stdio.h>
 
 char **pars_cmd(char *cmd) {
-  char **pcmd = ft_split(cmd, ' ');
-  return(pcmd);
+    char **pcmd = ft_split(cmd, ' ');
+    return (pcmd);
 }
 
-char *find_path(char **envp)  {
-  int i = 0;
-  int valid = -1;
-  while (envp[i]) {
-    valid = ft_strncmp(envp[i], "PATH=", 5);
-    if (valid == 0) return (envp[i] + 5);
-    i++;
-  }
-  return (NULL);
+char *find_path(char **envp) {
+    int i = 0;
+
+    while (envp[i]) {
+        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+            return (envp[i] + 5);
+        i++;
+    }
+    return (NULL);
 }
 
 char *find_cmd(char **path, char *cmd) {
-  int i = 0;
-  while (path[i]) {
-    char *gpath = ft_strjoin(path[i], cmd);
-    if ((access(gpath, X_OK)) == 0) {
-      printf("chemin pr la commande trouver -> %s\n", gpath);
-      return (gpath);
+    int i = 0;
+
+    while (path[i]) {
+        char *gpath = ft_strjoin(path[i], cmd);
+        if (access(gpath, X_OK) == 0)
+            return (gpath);
+        free(gpath);
+        i++;
     }
-    i++;
-  }
-  return (NULL);
+    return (NULL);
 }
 
 int main(int argc, char *argv[], char **envp) {
-  int pipe_fd[2];
-  if (argc != 5) {
-    write(1, "doit contenir 5 cmd -> f1 cmd1 cmd2 f2", 38);
-    return(1);
-  }
-  int fd_in = open(argv[1], O_RDONLY);
-  //printf("%i\n", fd_in);
-  if (fd_in == -1) {
-    perror("entree non valable");
+    int pipe_fd[2];
+
+    // Vérification des arguments
+    if (argc != 5) {
+        write(2, "Usage: ./pipex file1 cmd1 cmd2 file2\n", 38);
+        return (1);
+    }
+
+    // Ouverture du fichier d'entrée
+    int fd_in = open(argv[1], O_RDONLY);
+    if (fd_in == -1) {
+        perror("input file");
+        exit(1);
+    }
+
+    // Ouverture du fichier de sortie
+    int fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_out == -1) {
+        perror("output file");
+        close(fd_in);
+        exit(1);
+    }
+
+    // Récupération et parsing du PATH
+    char *path_env = find_path(envp);
+    if (!path_env) {
+        write(2, "PATH not found\n", 15);
+        exit(1);
+    }
+    char **path = ft_split(path_env, ':');
+
+    // Création du pipe
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    // Parsing et recherche de cmd1
+    char **pcmd1 = pars_cmd(argv[2]);
+    char *cmd1_path = find_cmd(path, pcmd1[0]);
+    if (!cmd1_path) {
+        write(2, "command not found: ", 19);
+        write(2, pcmd1[0], ft_strlen(pcmd1[0]));
+        write(2, "\n", 1);
+        exit(127);
+    }
+
+    // Premier fork (cmd1)
+    int pid1 = fork();
+    if (pid1 == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid1 == 0) {
+        // PROCESSUS FILS 1
+        // Redirections
+        dup2(fd_in, STDIN_FILENO);
+        dup2(pipe_fd[1], STDOUT_FILENO);
+
+        // Fermeture de tous les descripteurs
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        close(fd_in);
+        close(fd_out);
+
+        // Exécution
+        execve(cmd1_path, pcmd1, envp);
+
+        // Si on arrive ici, execve a échoué
+        perror("execve cmd1");
+        exit(127);
+    }
+
+    // Parsing et recherche de cmd2
+    char **pcmd2 = pars_cmd(argv[3]);
+    char *cmd2_path = find_cmd(path, pcmd2[0]);
+    if (!cmd2_path) {
+        write(2, "command not found: ", 19);
+        write(2, pcmd2[0], ft_strlen(pcmd2[0]));
+        write(2, "\n", 1);
+        exit(127);
+    }
+
+    // Deuxième fork (cmd2)
+    int pid2 = fork();
+    if (pid2 == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid2 == 0) {
+        // PROCESSUS FILS 2
+        // Redirections
+        dup2(pipe_fd[0], STDIN_FILENO);
+        dup2(fd_out, STDOUT_FILENO);
+
+        // Fermeture de tous les descripteurs
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        close(fd_in);
+        close(fd_out);
+
+        // Exécution
+        execve(cmd2_path, pcmd2, envp);
+
+        // Si on arrive ici, execve a échoué
+        perror("execve cmd2");
+        exit(127);
+    }
+
+    // PROCESSUS PARENT
+    // Fermeture de tous les descripteurs
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
     close(fd_in);
-    //printf("%i\n", fd_in);
-    exit(1);
-  }
-  int fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  char *path_env = find_path(envp);
-  char **path = ft_split(path_env, ':');
-  int i = 0;
-  /*while (path[i]) {
-    printf("%s\n", path[i]);
-    i++;
-  }*/
+    close(fd_out);
 
-  char *cmd1 = argv[2];
-  char **pcmd1 = pars_cmd(cmd1);
-  char *cmd_path = find_cmd(path, pcmd1[0]);
+    // Attente des deux processus fils
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
 
-  int a = pipe(pipe_fd);
-  printf("%i\n", a);
-
-  int pid1 = fork();
-  //int pid2 = fork();
-
-  if (pid1 == 0) {
-    printf("\n enffant n1 \n");
-    printf("%d",execve(cmd_path, pcmd1, NULL));
-  }
-  // if (pid2 == 0) {
-  //   printf("\n enffant n2 \n");
-  //   execv(pcmd1[1], NULL);
-  // }
-  int stat;
-  waitpid(pid1, &stat, 0);
-  
-  return 0;
+    return (0);
 }
