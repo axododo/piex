0fcd795351a5dd327f8886d2bdcfe28ab259701b
Mael Guilbert Costes <mguilber@3G6.42angouleme.fr>
2026-03-24 13:51:29 +0100

    del.o

--- archive/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char **argv, char **envp)
-{
-    int i = 0;
-    char *args[] = {"ls", "-la", NULL};
-    execve("/usr/bin/ls", args, envp);
-}
